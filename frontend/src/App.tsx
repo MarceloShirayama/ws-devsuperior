3071a21c3878c70db68947a85346dba5b4fb029b
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-09-06 11:28:16 -0300

    add Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import Navbar from "components/Navbar";
+
 function App() {
   return (
+    <>
+    <Navbar />
     <div>
       <h1 className="text-primary">Olá, semana Spring React!!!</h1>
     </div>
+    </>
   );
 }
 
