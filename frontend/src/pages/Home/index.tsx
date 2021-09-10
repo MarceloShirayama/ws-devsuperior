2327b056034d047d69a07e6724ea3a9e38643a8b
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-09-10 08:18:49 -0300

    add link in home

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import Footer from "components/Footer"
 import Navbar from "components/Navbar"
+import { Link } from "react-router-dom"
 
 function Home() {
   return (
@@ -11,6 +12,9 @@ function Home() {
         <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
         <hr />
         <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+        <Link to="/dashboard">
+          Acessar o dashboard
+        </Link>
       </div>
     </div>
     <Footer />
